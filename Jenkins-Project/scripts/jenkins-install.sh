0805f886459b935550616825775783f2c9fef361
SHIVAM PUJARI <128344968+Shiv6116@users.noreply.github.com>
2025-07-26 21:15:48 +0530

    Update jenkins-install.sh

--- Jenkins-Project/scripts/jenkins-install.sh
+++ Jenkins-Project/scripts/jenkins-install.sh
@@ -1,55 +1,27 @@
-#!/bin/bash
-# Install Git, Java, and Maven
+SETUP: CREATE EC2 WITH ALL TRAFFIC (8080)
+
+#STEP-1: INSTALLING GIT JAVA-1.8.0 MAVEN 
 yum install git java-1.8.0-openjdk maven -y
-SETUP:
 
-#! /bin/bash
-#Launch an instance with 9000 and t2.medium
-cd /opt/
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
-unzip sonarqube-8.9.6.50800.zip
+#STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+#STEP-3: DOWNLOAD JAVA11 AND JENKINS
 amazon-linux-extras install java-openjdk11 -y
-useradd sonar
-chown sonar:sonar sonarqube-8.9.6.50800 -R
-chmod 777 sonarqube-8.9.6.50800 -R
-su - sonar
+yum install jenkins -y
+update-alternatives --config java
 
-#run this on server manually
-#sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
-#echo "user=admin & password=admin"
+#STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
+systemctl start jenkins.service
+systemctl status jenkins.service
 
+copy public ip and paste on browser like this
+public-ip:8080
+cat /var/lib/jenkins/secrets/initialAdminPassword
+install plugins and create user for login.
 
-
-pipeline {
-    agent any 
-    
-    stages{
-        stage('checkout') {
-            steps {
-                git 'https://github.com/devopsbyraham/jenkins-java-project.git'
-            }
-        }
-        stage('build') {
-            steps {
-                sh 'mvn compile'
-            }
-        }
-        stage('test') {
-            steps {
-                sh 'mvn test'
-            }
-        }
-        stage('code quality') {
-            steps {
-                sh '''
-                sonarqube Script Past here
-                '''
-            }
-        }
-        stage('artifact') {
-            steps {
-                sh 'mvn clean package'
-            }
-        }
-    }
-}
+INTEGRATION OF GIT AND MAVEN WITH JENKINS:
+NEW ITEM -- > NAME: NETFLIX JOB -- > FREE STYLE -- > OK 
+Source Code Management -- > GIT -- > https://github.com/devopsbyraham/jenkins-java-project.git
+Build Steps -- > add setp -- > Execute shell -- > save
