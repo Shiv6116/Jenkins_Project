5b89bda94b9892e79a234792c1f101e307820f6f
SHIVAM PUJARI <128344968+Shiv6116@users.noreply.github.com>
2025-07-26 21:06:42 +0530

    Update tomcat-setup.sh

--- Jenkins-Project/scripts/tomcat-setup.sh
+++ Jenkins-Project/scripts/tomcat-setup.sh
@@ -11,3 +11,55 @@ TOMCAT SETUP:
 5. sh apache-tomcat-9.0.80/bin/startup.sh
 public-ip(in Server) ServerName:8080
 http://Public ip:8080/netflix/ 
+vim /root/apache-tomcat-9.0.80/conf/server.xml
+sh /root/apache-tomcat-9.0.80/bin/startup.sh
+sh /root/apache-tomcat-9.0.80/bin/shutdown.sh
+
+pipeline {
+    agent {
+        label 'slave1'
+    }
+    
+    stages {
+        stage('checkout') {
+            steps {
+                git 'https://github.com/devopsbyraham/jenkins-java-project.git'
+            }
+        }
+        stage('build') {
+            steps {
+                sh 'mvn compile'
+            }
+        }
+        stage('test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('code quality') {
+            steps {
+                sh '''
+                  Sonarqube Script Past here
+                '''
+            }
+        }
+        stage('artifact') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+        stage('deploy') {
+            steps {
+                deploy adapters:[
+                    tomcat9(
+                        credentialsId: '(Create ID Put Here)',
+                        path: '',
+                        url: 'http://Public ip:8080/'
+                        )
+                    ],
+                    contextPath: 'netflix',
+                    war: 'target/*.war'
+            }
+        }
+    }
+}
