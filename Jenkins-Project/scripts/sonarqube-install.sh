da5b54330fcbff244c6b41f2350fdb322146925b
SHIVAM PUJARI <128344968+Shiv6116@users.noreply.github.com>
2025-07-26 21:13:36 +0530

    Update sonarqube-install.sh

--- Jenkins-Project/scripts/sonarqube-install.sh
+++ Jenkins-Project/scripts/sonarqube-install.sh
@@ -1,5 +1,54 @@
-#!/bin/bash
-# Install SonarQube
+SETUP:
+
+#! /bin/bash
+#Launch an instance with 9000 and t2.medium
 cd /opt/
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
 unzip sonarqube-8.9.6.50800.zip
+amazon-linux-extras install java-openjdk11 -y
+useradd sonar
+chown sonar:sonar sonarqube-8.9.6.50800 -R
+chmod 777 sonarqube-8.9.6.50800 -R
+su - sonar
+
+#run this on server manually
+#sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
+#echo "user=admin & password=admin"
+
+after login -- > add project -- > manual -- > project key -- > netlfix -- > setup -- > token -- > netflix -- > generate -- > continue -- > maven 
+
+copy paste the code on pipeline:
+
+pipeline {
+    agent any 
+    
+    stages{
+        stage('checkout') {
+            steps {
+                git 'https://github.com/devopsbyraham/jenkins-java-project.git'
+            }
+        }
+        stage('build') {
+            steps {
+                sh 'mvn compile'
+            }
+        }
+        stage('test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('code quality') {
+            steps {
+                sh '''
+                      SonarQube Script Past Here
+                '''
+            }
+        }
+        stage('artifact') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+    }
+}
